c7b368ff2047da1e073b9cb75985b85e599cd4f1
Amit Dutta <damitlucky998@gmail.com>
2020-12-28 18:57:34 +0530

    Add files via upload

---
+++ Coronavirus Blocker Chrome Extension/script.js
@@ -0,0 +1,25 @@
+// alert("hi");
+
+replaceText(document.body)
+
+function replaceText(element){
+
+    if(element.hasChildNodes()){
+        element.childNodes.forEach(replaceText)
+    }else if (element.nodeType === Text.TEXT_NODE){
+        if(element.textContent.match(/coronavirus/gi)){
+            // const newElement=document.createElement('span')
+            // newElement.innerHTML=element.textContent.replace(/(coronavirus)/gi,
+            // '<span style="background-color:black; color:black;">$1</span>')
+            // element.replaceWith(newElement)
+            const newElement=document.createElement('span')
+            newElement.innerHTML=element.textContent.replace(/(coronavirus)/gi,
+            '<span class="rainbow">$1</span>')
+            element.replaceWith(newElement)
+            // element.parentElement.style.color='black'
+            // element.parentElement.style.background='black'
+            // element.parentElement.remove()
+        }
+        // element.textContent = element.textContent.replace(/coronavirus/gi,'LOL');
+    }
+}
